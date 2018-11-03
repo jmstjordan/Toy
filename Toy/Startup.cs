d70fdf5f5e81941fb937a1020053452db8971e45
JM Jordan <jord-osx@JMs-MacBook-Pro.local>
2018-11-03 14:51:44 -0600

    adding in check for file not found and malformed file

--- Toy/Startup.cs
+++ Toy/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
@@ -27,18 +28,8 @@ namespace toy
 
         public void ConfigureServices(IServiceCollection services)
         {
-            var userPath = _config.GetValue<string>("usersPath");
-            var groupPath = _config.GetValue<string>("groupsPath");
-
-            if(!string.IsNullOrEmpty(userPath))
-            {
-                UsersFilePath = userPath;
-            }
-            if(!string.IsNullOrEmpty(groupPath))
-            {
-                GroupFilePath = groupPath;
-            }
-
+            CheckFiles();
+            
             services.AddMvc();
             services.AddTransient<IGroupsService, GroupsService>((ctx) =>
             {
@@ -66,6 +57,96 @@ namespace toy
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "Toy API v1");
             });
+        }
+
+        private void CheckFiles()
+        {
+            var userPath = _config.GetValue<string>("usersPath");
+            var groupPath = _config.GetValue<string>("groupsPath");
+
+            if (!string.IsNullOrEmpty(userPath))
+            {
+                UsersFilePath = userPath;
+            }
+            if (!string.IsNullOrEmpty(groupPath))
+            {
+                GroupFilePath = groupPath;
+            }
+
+            if (!File.Exists(UsersFilePath))
+            {
+                throw new FileNotFoundException($"The input file: {UsersFilePath} passed in was not found");
+            }
+            if (!IsUsersMalformed(UsersFilePath))
+            {
+                throw new ArgumentException($"The input file: {UsersFilePath} passed in was not in the correct format.");
+            }
+            if (!File.Exists(GroupFilePath))
+            {
+                throw new FileNotFoundException($"The input file: {GroupFilePath} passed in was not found");
+            }
+            if (!IsGroupsMalformed(GroupFilePath))
+            {
+                throw new ArgumentException($"The input file: {GroupFilePath} passed in was not in the correct format.");
+            }
+        }
+
+        private bool IsGroupsMalformed(string filePath)
+        {
+            string line;
+            StreamReader file = new StreamReader(filePath);
+            while ((line = file.ReadLine()) != null)
+            {
+                if (line.StartsWith("#", StringComparison.Ordinal))
+                {
+                    continue;
+                }
+                if (line.Count(f => f == ':') != 3)
+                {
+                    return false;
+                }
+                var parts = line.Split(":");
+                int x;
+                if (!Int32.TryParse(parts[2], out x))
+                {
+                    return false;
+                }
+            }
+            file.Close();
+            return true;
+        }
+
+        private bool IsUsersMalformed(string filePath)
+        {
+            string line;
+            StreamReader file = new StreamReader(filePath);
+            while ((line = file.ReadLine()) != null)
+            {
+                if (line.StartsWith("#", StringComparison.Ordinal))
+                {
+                    continue;
+                }
+                if (string.IsNullOrWhiteSpace(line))
+                {
+                    return false;
+                }
+                if (line.Count(f => f == ':') != 6)
+                {
+                    return false;
+                }
+                var parts = line.Split(":");
+                int x;
+                if(!Int32.TryParse(parts[2], out x))
+                {
+                    return false;
+                }
+                if (!Int32.TryParse(parts[3], out x))
+                {
+                    return false;
+                }
+            }
+            file.Close();
+            return true;
         }
     }
 }
